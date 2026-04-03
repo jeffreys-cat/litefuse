e485630df82772ab7a4e3b6476b69ee1317f1a16
Jeffrey <color.dove@gmail.com>
2026-04-03 11:15:58 +0800

    feat: hide the add to datasets, annotate

--- web/src/components/trace2/TracePreview.tsx
+++ web/src/components/trace2/TracePreview.tsx
@@ -206,32 +206,9 @@ export const TracePreview = ({
             <CopyIdsPopover idItems={[{ id: trace.id, name: "Trace ID" }]} />
           </div>
           <div className="flex h-full flex-wrap content-start items-start justify-start gap-0.5 @2xl:mr-1 @2xl:justify-end">
-            <NewDatasetItemFromExistingObject
-              traceId={trace.id}
-              projectId={trace.projectId}
-              input={trace.input}
-              output={trace.output}
-              metadata={trace.metadata}
-              key={trace.id}
-              size="sm"
-            />
             {viewType === "detailed" && (
               <>
                 <div className="flex items-start">
-                  <AnnotateDrawer
-                    key={"annotation-drawer" + trace.id}
-                    projectId={trace.projectId}
-                    scoreTarget={{
-                      type: "trace",
-                      traceId: trace.id,
-                    }}
-                    scores={scores}
-                    scoreMetadata={{
-                      projectId: trace.projectId,
-                      environment: trace.environment,
-                    }}
-                    size="sm"
-                  />
                   <CreateNewAnnotationQueueItem
                     projectId={trace.projectId}
                     objectId={trace.id}
